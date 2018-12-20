e8773fa613f98cff3825f1aad8560a0aa178fb7f
Angel <bybortexx@gmail.com>
2018-12-20 13:06:27 +0100

    sprint1 acabado

--- public_html/php/app/controllers/IndexController.php
+++ public_html/php/app/controllers/IndexController.php
@@ -2,11 +2,6 @@
 
 class IndexController extends core\MVC\Action {
 
-    public function IndexAction() {
-        $this->setView("portada");
-        $this->renderView();
-        
-    }
 
     public function CategoriasAction() {
         $model = $this->getModel('categorias', 'categorias');
@@ -17,29 +12,5 @@ class IndexController extends core\MVC\Action {
         $model = $this->getModel('carrusel', 'carrusel');
         $data = $model->select('*');
         echo \json_encode($data);
-    }
-
-    public function HistoriaAction() {
-        $this->setView("historia");
-        $this->renderView();
-    }
-
-    public function JugadoresAction() {
-        $model = $this->getModel('jugadores', 'jugadores');
-        $data = $model->select('codigo, nombre, foto', 'Nombre_equipo="Lakers"');
-        $this->setView("jugadores");
-        $this->renderView($data);
-    }
-
-    public function JugadoresEquipoAction() {
-        $equipo = $this->getParam("idEquipo");
-        $model = $this->getModel('jugadores', 'jugadores');
-        $data = $model->select('codigo, nombre, foto', 'Nombre_equipo="' . $equipo .  '"');
-        echo \json_encode($data);
-    }
-
-    public function PartidosAction() {
-        $this->setView("partidos");
-        $this->renderView();
     }
 }
