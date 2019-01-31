b56f52382e24e4de612907c671aa7ef0bf94eafd
Angel <bybortexx@gmail.com>
2019-01-31 09:53:23 +0100

    error ultima hora

--- public_html/api/app/resources/RegistroResource.php
+++ public_html/api/app/resources/RegistroResource.php
@@ -23,14 +23,10 @@ class RegistroResource extends Resource {
         $this->execSQL();
         if ($this->num_rows === 0) {
             if (filter_var($correo, FILTER_VALIDATE_EMAIL)) { 
-        try{
             $this->sql ="INSERT INTO `usuarios` (nombreUsuario,contrasenya,nombre,apellidos,direccion,codigoPostal,correoElectronico) 
             VALUES ('$usuario', '$contrasenya', '$nombre', '$apellidos', '$direccion', '$codigoPostal', '$correo')";
             $this->execSQL();   
-        } catch (PDOException $e) {
-            http_response_code(500);
-            exit();
-        }
+
         } else {
             http_response_code(400);
             }
