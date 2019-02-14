8e2af114303639b6d39290975038f0d0b86762c7
Angel <bybortexx@gmail.com>
2019-02-14 22:53:33 +0100

    deploy change

--- deploy/buildFrance.sh
+++ deploy/buildFrance.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 node-sass ../public_html/scss/*.scss -o ../public_html/css
 tsc ../public_html/js/*.ts
 rm -r ../../../www/*
 cp -r ../public_html/* ../../../www/
 rm -r ../../../www/scss
 rm -f ../../../www/js/*.ts
-
